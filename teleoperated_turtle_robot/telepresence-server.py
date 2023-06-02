a34d925a2fa77ed69c76a7cec009f658c0f45e0b
hphilamore <hemma.philamore@bristol.ac.uk>
2023-06-02 17:26:50 +0100

    server updated to trasmit coordinates not commands

--- teleoperated_turtle_robot/telepresence-server.py
+++ teleoperated_turtle_robot/telepresence-server.py
@@ -31,94 +31,80 @@ time_old = time()
 flag_wings = True
 
 
-def pos_to_command(x, y):
-    """
-    Translates position of hand detected to command sent to robot
-    """
-    if 0.0 < x < 1.0:        # Check hand detected in frame        
 
-        if x < 0.4:        # Turn left
-            out = 'left'
-             
-        elif x > 0.6:        # Turn right 
-            out = 'right'
-            
-        else:                # Go forwards
-            if y >= 0.5:
-                out = 'backward'
-            else:
-                out = 'forward'
+while(1):
 
-    else:
-        out = 'none'
-
-    return out
-
-
-conn, addr = server_socket.accept()
-with conn:
-    print(f"Connected by {addr}")
-
-
-
-while True:
-    # Switch wing direction every 3s
+    # # Switch wing direction every 3s
     # time_new = time()
-    # if time_new-time_old >= 2:
-    #     flag_wings = not flag_wings
+    # if time_new-time_old >= 3:
     #     time_old = time_new
     #     print('switched wing direction')
-    #     #sleep(2)
 
-    # # # Switch wings on/off
-    #     if flag_wings:
-    #         print('wings up')
-    #         motor3.forward()
-    #         motor4.forward()
-    #     else:
-    #         print('wings down')
-    #         motor3.stop()
-    #         motor4.stop()
+    # # Switch wings on/off
+    # if flag_wings:
+    #     print('wings up')
+    #     motor3.forward()
+    #     motor4.forward()
+    # else:
+    #     print('wings down')
+    #     motor3.stop()
+    #     motor4.stop()
 
-    data = conn.recv(1024)
-    if not data:
-        break
-    msg = data.decode()
-    print(msg)
+    # print('looper')
 
-    # if msg != 'no hand' and msg != 'stop':
-    if msg not in ['no hand', 'stop', 'forward', 'backward', 'right', 'left']:
 
-        coordinates = msg.split(',')
 
-        coordinates = [float(i) for i in coordinates]
+    conn, addr = server_socket.accept()
+    with conn:
+        print(f"Connected by {addr}")
 
-        # Grouped coordintes
-        coordinates = [coordinates[i:i+2] for i in range(0, len(coordinates), 2)]
 
-        print(coordinates)
 
-        msg = pos_to_command(coordinates[0], coordinates[1])
+        while True:
 
-    if msg == 'stop':
-        motor1.stop()
-        motor2.stop()
-        motor3.stop()
-        motor4.stop()
+            # time_new = time()
+            # if time_new-time_old >= 2:
+            #     flag_wings = not flag_wings
+            #     time_old = time_new
+                        #     #sleep(2)
 
-    elif msg == 'left':
-        motor1.stop()
-        motor2.forward(0.5)
+            # # # Switch wings on/off
+            #     if flag_wings:
+            #         print('wings up')
+            #         motor3.forward()
+            #         motor4.forward()
+            #     else:
+            #         print('wings down')
+            #         motor3.stop()
+            #         motor4.stop()
 
-    elif msg == 'right':
-        motor1.forward(0.5)
-        motor2.stop()
-        
 
-    elif msg == 'forward':
-        motor1.forward(0.5)
-        motor2.forward(0.5)
 
+
+            data = conn.recv(1024)
+            if not data:
+                break
+            msg = data.decode()
+            print(msg)
+
+            if msg == 'stop':
+                motor1.stop() 
+                motor2.stop()
+                motor3.stop() 
+                motor4.stop()
+
+            elif msg == 'left':
+                motor1.stop()
+                motor2.forward()
+
+            elif msg == 'right':
+                motor1.forward()
+                motor2.stop()
+                
+            elif msg == 'forward':
+                motor1.forward()
+                motor2.forward()
+                
             #conn.sendall(data)
     
     # except:
