c4cba2a0ec40d672266d85c3a3437da56a20e349
hphilamore <hemma.philamore@bristol.ac.uk>
2023-07-03 19:46:17 +0100

    tidied up new code

--- teleoperated_wings_robot/ax12_servo_autonomous_sequence_.py
+++ teleoperated_wings_robot/ax12_servo_autonomous_sequence_.py
@@ -15,38 +15,56 @@ rx_pin = 15
 GPIO.setup(tx_pin, GPIO.OUT)
 GPIO.setup(rx_pin, GPIO.IN)
 
-def read_servo_position(servo_id):
-    # Open the serial port
-    ser = serial.Serial(SERIAL_PORT, baudrate=BAUDRATE)
+# Create serial object 
+Dynamixel=serial.Serial("/dev/serial0",
+                        baudrate=1000000,
+                        timeout=0.1, 
+                        bytesize=8)   # UART in ttyS0 @ 1Mbps
 
-    # Construct the instruction packet
-    packet = bytearray([0xFF, 0xFF, servo_id, 0x04, 0x02, 0x24, 0x01])
+Dynamixel.write(bytearray.fromhex("FF FF 01 04 02 24 02 D2"))   # Read Position of Servo with ID = 1
+GPIO.output(18,GPIO.LOW)
+startDynamixel = Dynamixel.read()
+startDynamixel = Dynamixel.read()
+idDynamixel = Dynamixel.read()
+lenghtDynamixel = Dynamixel.read()
+errorDynamixel = Dynamixel.read()
+posDynamixel = Dynamixel.read(2)
+chkDynamixel = Dynamixel.read()
+print("Servo Position = " , int.from_bytes(posDynamixel,byteorder='little'))
+time.sleep(1)
 
-    # Calculate the checksum
-    checksum = (~(servo_id + 0x04 + 0x02 + 0x24) & 0xFF)
+# def read_servo_position(servo_id):
+#     # Open the serial port
+#     ser = serial.Serial(SERIAL_PORT, baudrate=BAUDRATE)
 
-    # Add the checksum to the packet
-    packet.append(checksum)
+#     # Construct the instruction packet
+#     packet = bytearray([0xFF, 0xFF, servo_id, 0x04, 0x02, 0x24, 0x01])
 
-    # Send the instruction packet
-    ser.write(packet)
+#     # Calculate the checksum
+#     checksum = (~(servo_id + 0x04 + 0x02 + 0x24) & 0xFF)
 
-    # Wait for the response packet
-    time.sleep(0.1)
+#     # Add the checksum to the packet
+#     packet.append(checksum)
 
-    # Read the response packet
-    response = ser.read(7)
+#     # Send the instruction packet
+#     ser.write(packet)
 
-    # Close the serial port
-    ser.close()
+#     # Wait for the response packet
+#     time.sleep(0.1)
 
-    # Check if the response packet is valid
-    if len(response) == 7 and response[0] == 0xFF and response[1] == 0xFF and response[2] == servo_id:
-        # Extract the present position from the response packet
-        present_position = response[5] + (response[6] << 8)
-        print(f"Servo ID {servo_id} present position: {present_position} degrees")
-    else:
-        print(f"Failed to read present position for Servo ID {servo_id}")
+#     # Read the response packet
+#     response = ser.read(7)
 
-# Example usage
-read_servo_position(0x03)  # Replace with the appropriate servo ID
+#     # Close the serial port
+#     ser.close()
+
+#     # Check if the response packet is valid
+#     if len(response) == 7 and response[0] == 0xFF and response[1] == 0xFF and response[2] == servo_id:
+#         # Extract the present position from the response packet
+#         present_position = response[5] + (response[6] << 8)
+#         print(f"Servo ID {servo_id} present position: {present_position} degrees")
+#     else:
+#         print(f"Failed to read present position for Servo ID {servo_id}")
+
+# # Example usage
+# read_servo_position(0x03)  # Replace with the appropriate servo ID
