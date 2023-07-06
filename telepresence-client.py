52f62e883e83deb6c3411c4a90d5262915795f91
hphilamore <hemma.philamore@bristol.ac.uk>
2023-07-06 17:18:25 +0100

    deleted commented out lines

--- telepresence-client.py
+++ telepresence-client.py
@@ -254,19 +254,6 @@ elif input_mode == 'window':
     # Set up window for image capture
     window_coordinates = window_coordinates()
      
-    # process = Popen(['./windowlist', 'windowlist.m'], stdout=PIPE, stderr=PIPE)
-    # stdout, stderr = process.communicate()
-    # window_positions = stdout.decode().split('\n')
-
-    # for w in window_positions:
-    #     if win_name in w:                        # Find window 
-    #         print(w)
-    #         w = w.split(':')                     # Separate window info 
-    #         print(w)
-    #         coordinates = w[-1].split(',')       # Separate window coordinates
-    #         print(coordinates)
-    #         coordinates = [int(float(i)) for i in coordinates]  # Convert coordinates to integer
-    #         print(coordinates)
 
 elif input_mode == 'camera':
     """ Setup web cam ready for video capture """
@@ -283,115 +270,14 @@ while(True):
 
         # Input taken from window
         if input_mode == 'window':
-            frame = frame_from_window(window_coordinates)
-
-            # with mss() as sct:
-
-            #     try:
-            #         # Use coordinates of window
-            #         # with mss() as sct:
-            #         window = {"top": window_coordinates[1], 
-            #               "left": window_coordinates[0], 
-            #               "width": window_coordinates[3], 
-            #               "height": window_coordinates[2]
-            #                }
-
-            #         # Grab current image    
-            #         frame = np.array(sct.grab(window))
-
-            #         # If full screen image grab required
-            #         if grab_full_screen_image: 
-            #             frame = np.array(ImageGrab.grab()) 
-
-            #         frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
-
-            #     except:
-            #         print("No window with specified name")
-            #         print("Exiting program...")
-            #         sys.exit(1)
-            
+            frame = frame_from_window(window_coordinates)    
 
         elif input_mode == 'camera':
-            """
-            
-            """
             frame = frame_from_camera(capture)
-            # ret, frame = capture.read()
-            # # Grab current image    
-            # frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-            # frame = cv2.flip(frame, 1)
 
 
         # Look for hands 
         command = track_hands(frame, flag_no_hand, flag_timeout) 
-
-
-        # results = hands.process(frame)
-
-        # # Check for hands
-        # if results.multi_hand_landmarks != None:
-
-        #     # Draw hands
-        #     for handLandmarks in results.multi_hand_landmarks:
-        #         # Draw landmarks onto frame 
-        #         drawingModule.draw_landmarks(frame, 
-        #                                      handLandmarks, 
-        #                                      handsModule.HAND_CONNECTIONS)
-
-        #     hand_coordinates = []
-
-        #     # Find each hand up to max number of hands 
-        #     for hand_no, hand_landmarks in enumerate(results.multi_hand_landmarks):
-        #         print(f'HAND NUMBER: {hand_no+1}')
-        #         print('-----------------------')
- 
-        #         x_ = []
-        #         y_ = []
-        #         z_ = []
-
-        #         for i in range(20):
-        #             x_.append(hand_landmarks.landmark[handsModule.HandLandmark(i).value].x)
-        #             y_.append(hand_landmarks.landmark[handsModule.HandLandmark(i).value].y)
-        #             z_.append(hand_landmarks.landmark[handsModule.HandLandmark(i).value].z)
-                        
-        #         # Find mean value of x and z coordinate of nodes 
-        #         x = sum(x_)/len(x_)                
-        #         y = sum(y_)/len(y_)                
-        #         z = sum(z_)/len(z_)
-
-        #         print(x, y, z)
-
-        #         # Add the mean values to the list of coordinates to send to raspberry pi
-        #         hand_coordinates.append(str(round(x, 2)))
-        #         hand_coordinates.append(str(round(y, 2)))
-
-        #     # Choose a command to send to the raspberry pi robot 
-        #     # command = pos_to_command(x, y, z)
-        #     command = ','.join(hand_coordinates)
-        #     print(command)
-
-        # else:
-        #         print('no hand')
-        #         if not flag_no_hand:     # If there was a hand in previous frame
-        #             flag_no_hand = True  # Raise the flag 
-        #             start = time.time()  # Start the timer
-        #             command = 'no command'
-
-        #         else:
-        #             end = time.time()
-        #             if end-start >= flag_timeout:
-        #                 flag_no_hand = False  # Lower the flag 
-        #                 print('stop')
-        #                 command = 'stop'  
-
-
-        # if send_command:
-        #     # Send command to server socket on raspberry pi
-        #     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        #         # s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) # Allow reuse of address
-        #         s.connect((HOST, PORT))
-        #         s.sendall(command.encode())
-
 
         if OS == 'windowsOS': 
             if make_output_window_fullscreen:
