31d7a7f8b98d7dcc0f9a2f98c93d6e54f867d162
hphilamore <hemma.philamore@bristol.ac.uk>
2023-09-22 11:11:54 +0100

    reverted to code cleanup verison of dictionary communication code

--- teleoperated_wings_robot/telepresence-server.py
+++ teleoperated_wings_robot/telepresence-server.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 
-
 import socket
 # from gpiozero import Motor, OutputDevice
 from time import sleep
@@ -112,172 +111,187 @@ def hand_speed(buffer):
     return speed
 
 
+# Loop forever checking for mode button press and/or connection by remote client  
 while True:
-    # Teleoperated mode button pressed
-    if (GPIO.input(teleop_mode_button) == GPIO.HIGH and 
-        GPIO.input(preprog_mode_button) == GPIO.LOW):
-        print("Teleoperated")
-        # Turn button pressed LED on and other LED off
-        GPIO.output(teleop_mode_LED,GPIO.HIGH)
-        GPIO.output(preprog_mode_LED,GPIO.LOW)
 
-        try:
+    # # Teleoperated mode button pressed
+    # if (GPIO.input(teleop_mode_button) == GPIO.HIGH and 
+    #     GPIO.input(preprog_mode_button) == GPIO.LOW):
+    #     print("Teleoperated")
+    #     # Turn button pressed LED on and other LED off
+    #     GPIO.output(teleop_mode_LED,GPIO.HIGH)
+    #     GPIO.output(preprog_mode_LED,GPIO.LOW)
 
-            conn, addr = server_socket.accept()
-            with conn:
-                # print(f"Connected by {addr}")
+    # Listen for message from client 
+    try:
+        conn, addr = server_socket.accept()
+        with conn:
+            print(f"Connected by {addr}")
 
-                # Teleoperation mode
-                while (GPIO.input(teleop_mode_button) == GPIO.HIGH and 
-                       GPIO.input(preprog_mode_button) == GPIO.LOW):
+            # Teleoperated mode button pressed
+            while (GPIO.input(teleop_mode_button) == GPIO.HIGH and 
+                   GPIO.input(preprog_mode_button) == GPIO.LOW):
 
-                    set_endless(0x03, False, Dynamixel)
-                    set_endless(0x04, False, Dynamixel)
-                    set_endless(0x02, False, Dynamixel)
-                    set_endless(0x01, False, Dynamixel)
-                    GPIO.output(18,GPIO.HIGH)
+                print("Teleoperated Mode")
 
-                    data = conn.recv(1024)
-                    if not data:
-                        break
-                    msg = data.decode()
+                # Turn button pressed LED on and other LED off
+                GPIO.output(teleop_mode_LED,GPIO.HIGH)
+                GPIO.output(preprog_mode_LED,GPIO.LOW)
 
-                    if msg not in ['no command', 'stop', 'forward', 'backward', 'right', 'left']:
+                set_endless(0x03, False, Dynamixel)
+                set_endless(0x04, False, Dynamixel)
+                set_endless(0x02, False, Dynamixel)
+                set_endless(0x01, False, Dynamixel)
+                GPIO.output(18,GPIO.HIGH)
 
-                        # convert string-dictionary of node coordinates to dictionary
-                        msg = json.loads(msg)
+                data = conn.recv(1024)
+                if not data:
+                    break
+                msg = data.decode()
 
-                        print('msg2', type(msg), msg)
+                if msg not in ['no command', 'stop', 'forward', 'backward', 'right', 'left']:
 
-                        # array to store x,y,z coordinates for each hand  
-                        hands = ["RIGHT_WRIST", "LEFT_WRIST"]
+                    # convert string-dictionary of node coordinates to dictionary
+                    msg = json.loads(msg)
 
-                        # For each hand [left, right]
-                        for hand, motors, buffer, side in zip(hands, 
-                                                           [motors_right, motors_left], 
-                                                           [buffer_right, buffer_left]):
-                            
-                            x_pos = msg[hand][x]
-                            y_pos = msg[hand][y]
+                    print('msg2', type(msg), msg)
 
-                            print('x pos ', x_pos)
-                            print('y pos ', y_pos)
+                    # array to store x,y,z coordinates for each hand  
+                    hands = ["RIGHT_WRIST", "LEFT_WRIST"]
 
-                            # Input range for horizontal position of left and right hand 
-                            # (absolute min=0, absolute max=1)
-                            min_in_L = 0
-                            max_in_L = 0.75
-                            min_in_R = 0.35
-                            max_in_R = 1
+                    # For each hand [left, right]
+                    for hand, motors, buffer, side in zip(hands, 
+                                                       [motors_right, motors_left], 
+                                                       [buffer_right, buffer_left]):
+                        
+                        x_pos = msg[hand][x]
+                        y_pos = msg[hand][y]
 
-                            # Input range for vertical position of left and right hand 
-                            # (absolute min=0, absolute max=1)
-                            min_in_L = 0
-                            max_in_L = 0.75
-                            min_in_R = 0.35
-                            max_in_R = 1
+                        print('x pos ', x_pos)
+                        print('y pos ', y_pos)
 
-                            # Output range for horizontal position servos 
-                            # (absolute min=0, absolute max=1023)
-                            min_out_L = 300#512
-                            max_out_L = 1023
-                            min_out_R = 0
-                            max_out_R = 700#512
+                        # Input range for horizontal position of left and right hand 
+                        # (absolute min=0, absolute max=1)
+                        min_in_L = 0
+                        max_in_L = 0.75
+                        min_in_R = 0.35
+                        max_in_R = 1
 
-                            # Output range for vertical position servos 
-                            # (absolute min=0, absolute max=1023)
-                            min_in_V = 0.25
-                            max_in_V = 1
-                            min_out_V = 0
-                            max_out_V = 600
+                        # Input range for vertical position of left and right hand 
+                        # (absolute min=0, absolute max=1)
+                        min_in_L = 0
+                        max_in_L = 0.75
+                        min_in_R = 0.35
+                        max_in_R = 1
 
-                            # Map horizontal pose to servos  
-                            if side == 'right':
-                                if x_pos<=min_in_R: x_pos = min_in_R
-                                h_position = (min_out_R + (max_out_R - min_out_R) * (x_pos-min_in_R) / 
-                                             (max_in_R - min_in_R))
-                            elif side == 'left': 
-                                if x_pos>=max_in_L: x_pos = max_in_L
-                                h_position = (min_out_L + (max_out_L - min_out_L) * (x_pos-min_in_L) / 
-                                             (max_in_L - min_in_L))
+                        # Output range for horizontal position servos 
+                        # (absolute min=0, absolute max=1023)
+                        min_out_L = 300#512
+                        max_out_L = 1023
+                        min_out_R = 0
+                        max_out_R = 700#512
 
-                            h_position = int(h_position)
+                        # Output range for vertical position servos 
+                        # (absolute min=0, absolute max=1023)
+                        min_in_V = 0.25
+                        max_in_V = 1
+                        min_out_V = 0
+                        max_out_V = 600
 
-                            # Map vertical pose to servos 
-                            y_pos = 1 - y_pos # Correct position to account for mirrored servo arrangement
-                            if y_pos<=min_in_V: y_pos = min_in_V
-                            v_position = (min_out_V + (max_out_V - min_out_V) * (y_pos-min_in_V) / 
-                                         (max_in_V - min_in_V))
-                            v_position = int(v_position)
+                        # Map horizontal pose to servos  
+                        if side == 'right':
+                            if x_pos<=min_in_R: x_pos = min_in_R
+                            h_position = (min_out_R + (max_out_R - min_out_R) * (x_pos-min_in_R) / 
+                                         (max_in_R - min_in_R))
+                        elif side == 'left': 
+                            if x_pos>=max_in_L: x_pos = max_in_L
+                            h_position = (min_out_L + (max_out_L - min_out_L) * (x_pos-min_in_L) / 
+                                         (max_in_L - min_in_L))
 
-                            # Moving average filter applied, Position rounded to nearest decimal value
-                            h_smoothed = int(moving_average(new_val = h_position, buffer[h])) 
-                            v_smoothed = int(moving_average(new_val = v_position, buffer[v])) 
+                        h_position = int(h_position)
 
-                            # Scale speed of servo to speed of hand in horizontal and vertical direction
-                            h_speed = hand_speed(buffer[h]) 
-                            v_speed = hand_speed(buffer[v])                  
+                        # Map vertical pose to servos 
+                        y_pos = 1 - y_pos # Correct position to account for mirrored servo arrangement
+                        if y_pos<=min_in_V: y_pos = min_in_V
+                        v_position = (min_out_V + (max_out_V - min_out_V) * (y_pos-min_in_V) / 
+                                     (max_in_V - min_in_V))
+                        v_position = int(v_position)
 
-                            # Send 10-bit value to servos controlling horizontal and veritcal motion 
-                            move_speed(motors[0], h_smoothed, h_speed, Dynamixel)
-                            move_speed(motors[1], v_smoothed, v_speed, Dynamixel)
-                            print('h_servo= ', h_smoothed, ' v_servo= ', v_smoothed)
-                            print()
+                        # Moving average filter applied, Position rounded to nearest decimal value
+                        h_smoothed = int(moving_average(new_val = h_position, buffer[h])) 
+                        v_smoothed = int(moving_average(new_val = v_position, buffer[v])) 
 
-                            # Servo control resolution is coarse
-                            if coarse_servo_v_position:
-                                if v_smoothed<0.35:
-                                    print('up')
-                                    move_speed(motors[0], 1023, 500, Dynamixel)
-                                elif 0.35<=v_smoothed<0.65:
-                                    move_speed(motors[0], 512, 500, Dynamixel)
-                                    print('mid')
-                                else:
-                                    move_speed(motors[0], 0, 500, Dynamixel)
-                                    print('down')
+                        # Scale speed of servo to speed of hand in horizontal and vertical direction
+                        h_speed = hand_speed(buffer[h]) 
+                        v_speed = hand_speed(buffer[v])                  
 
-                    if msg == 'stop':
-                        pass
+                        # Send 10-bit value to servos controlling horizontal and veritcal motion 
+                        move_speed(motors[0], h_smoothed, h_speed, Dynamixel)
+                        move_speed(motors[1], v_smoothed, v_speed, Dynamixel)
+                        print('h_servo= ', h_smoothed, ' v_servo= ', v_smoothed)
+                        print()
 
-                    elif msg == 'left':
-                        move(0x04, 0, Dynamixel)
-                        move(0x03, 0, Dynamixel)
-                        move(0x02, 0, Dynamixel)
-                        move(0x01, 0, Dynamixel)
-                        sleep(0.1)
+                        # Servo control resolution is coarse
+                        if coarse_servo_v_position:
+                            if v_smoothed<0.35:
+                                print('up')
+                                move_speed(motors[0], 1023, 500, Dynamixel)
+                            elif 0.35<=v_smoothed<0.65:
+                                move_speed(motors[0], 512, 500, Dynamixel)
+                                print('mid')
+                            else:
+                                move_speed(motors[0], 0, 500, Dynamixel)
+                                print('down')
 
-                    elif msg == 'right':
-                        move(0x04, 150, Dynamixel)
-                        move(0x03, 150, Dynamixel)
-                        move(0x02, 150, Dynamixel)
-                        move(0x01, 150, Dynamixel)
-                        sleep(0.1)
+                if msg == 'stop':
+                    pass
 
-                    elif msg == 'forward':
-                        pass
+                elif msg == 'left':
+                    move(0x04, 0, Dynamixel)
+                    move(0x03, 0, Dynamixel)
+                    move(0x02, 0, Dynamixel)
+                    move(0x01, 0, Dynamixel)
+                    sleep(0.1)
 
-        except socket.timeout:
-            pass
+                elif msg == 'right':
+                    move(0x04, 150, Dynamixel)
+                    move(0x03, 150, Dynamixel)
+                    move(0x02, 150, Dynamixel)
+                    move(0x01, 150, Dynamixel)
+                    sleep(0.1)
 
-    # Autonomous mode button pressed
-    elif (GPIO.input(preprog_mode_button) == GPIO.HIGH and 
+                elif msg == 'forward':
+                    pass
+
+    # No message from client 
+    except socket.timeout:
+        pass
+
+    # # Autonomous mode button pressed
+    # elif (GPIO.input(preprog_mode_button) == GPIO.HIGH and 
+    #       GPIO.input(teleop_mode_button) == GPIO.LOW):
+    #     print("Autonomous mode")
+    #     # Turn button pressed LED on and other LED off
+    #     GPIO.output(teleop_mode_LED,GPIO.LOW)
+    #     GPIO.output(preprog_mode_LED,GPIO.HIGH)
+
+    # Autonomous mode 
+    while(GPIO.input(preprog_mode_button) == GPIO.HIGH and 
           GPIO.input(teleop_mode_button) == GPIO.LOW):
+
         print("Autonomous mode")
+
         # Turn button pressed LED on and other LED off
         GPIO.output(teleop_mode_LED,GPIO.LOW)
         GPIO.output(preprog_mode_LED,GPIO.HIGH)
 
-        # Autonomous mode 
-        while(GPIO.input(preprog_mode_button) == GPIO.HIGH and 
-              GPIO.input(teleop_mode_button) == GPIO.LOW):
-
-            preprogrammed_motion(motor_right_v, 
-                                 motor_left_v, 
-                                 motor_right_h, 
-                                 motor_left_h)
+        preprogrammed_motion(motor_right_v, 
+                             motor_left_v, 
+                             motor_right_h, 
+                             motor_left_h)
 
     # Both teleoperated and Autonomous mode button pressed
-    elif (GPIO.input(preprog_mode_button) == GPIO.HIGH and 
+    while (GPIO.input(preprog_mode_button) == GPIO.HIGH and 
           GPIO.input(teleop_mode_button) == GPIO.HIGH):
         # Turn both LEDs on
         GPIO.output(teleop_mode_LED,GPIO.HIGH)
@@ -285,8 +299,7 @@ while True:
 
 
     # No button pressed
-    else:
-        # Turn both LEDs off
-        GPIO.output(teleop_mode_LED,GPIO.LOW)
-        GPIO.output(preprog_mode_LED,GPIO.LOW)
+    # Turn both LEDs off
+    GPIO.output(teleop_mode_LED,GPIO.LOW)
+    GPIO.output(preprog_mode_LED,GPIO.LOW)
 
