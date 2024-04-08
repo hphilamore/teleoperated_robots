086cd9444b17c1102479403346cd0a7cb683a5b3
hphilamore <hemma.philamore@bristol.ac.uk>
2024-04-08 22:52:43 +0100

    leap motion tracking class moved to seperate file

--- telepresence-client.py
+++ telepresence-client.py
@@ -20,8 +20,9 @@ import numpy as np
 import curses
 import json
 import leap
-import time
 import platform
+from leap_motion_tracking import *
+
 
 #--------------------------------""" SETUP """-----------------------------------------------
 
@@ -29,7 +30,7 @@ HOST = "192.168.0.53"      # The raspberry pi's hostname or IP address
 PORT = 65448               # The port used by the server
 
 # Source of input: 'leap_motion', 'camera' or 'window' or 'keys'
-input_mode = 'camera'#'window'#'leap_motion'# 'camera' ##'camera'#'keys' 
+input_mode = 'leap_motion'#'camera'#'window'#'leap_motion'# 'camera' ##'camera'#'keys' 
 
 # Set to True to send command to raspberry pi
 send_command = False
@@ -49,101 +50,101 @@ if OS == 'Windows':
 # mp_drawing = mediapipe.solutions.drawing_utils
 # mp_pose = mediapipe.solutions.pose
 
-class LeapMotionTracker(leap.Listener):
+# class LeapMotionTracker(leap.Listener):
 
-    def __init__(self):
-        self.command = 'stop'
+#     def __init__(self):
+#         self.command = 'stop'
 
-    def on_connection_event(self, event):
-        print("Connected")
+#     def on_connection_event(self, event):
+#         print("Connected")
 
-    def on_device_event(self, event):
-        try:
-            with event.device.open():
-                info = event.device.get_info()
-        except leap.LeapCannotOpenDeviceError:
-            info = event.device.get_info()
+#     def on_device_event(self, event):
+#         try:
+#             with event.device.open():
+#                 info = event.device.get_info()
+#         except leap.LeapCannotOpenDeviceError:
+#             info = event.device.get_info()
 
-        print(f"Found device {info.serial}")
+#         print(f"Found device {info.serial}")
 
-    def send_command_to_server(self, HOST, PORT):
-        """
-        Uses sockets to send command to server robot over local network
-        """
-        # Send command to server socket on raspberry pi
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            s.connect((HOST, PORT))
-            s.sendall(self.command.encode())
+#     def send_command_to_server(self, HOST, PORT):
+#         """
+#         Uses sockets to send command to server robot over local network
+#         """
+#         # Send command to server socket on raspberry pi
+#         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+#             s.connect((HOST, PORT))
+#             s.sendall(self.command.encode())
 
-    def hand_coordinates_to_command(self, pose_coordinates):
-        # TODO: Cusrrent bug, comparing lists. Need to compare coordiates values! 
+#     def hand_coordinates_to_command(self, pose_coordinates):
+#         # TODO: Cusrrent bug, comparing lists. Need to compare coordiates values! 
 
-        """
-        Converts hand pose coordinates to robot commands 
-        """
+#         """
+#         Converts hand pose coordinates to robot commands 
+#         """
 
-        # Set a threshold indicating the midpoint of the horizontal and vertical range 
-        vertical_th = 220
-        horizontal_th = 0
+#         # Set a threshold indicating the midpoint of the horizontal and vertical range 
+#         vertical_th = 220
+#         horizontal_th = 0
 
-        # Default command to send 
-        self.command = 'no command'
+#         # Default command to send 
+#         self.command = 'no command'
 
-        # Initialize arrays to store coordinates of each hand 
-        left, right = [], []
+#         # Initialize arrays to store coordinates of each hand 
+#         left, right = [], []
 
-        # Replace with recievd coordinates 
-        try:
-            left = pose_coordinates["LEFT_HAND"]
-        except:
-            pass
+#         # Replace with recievd coordinates 
+#         try:
+#             left = pose_coordinates["LEFT_HAND"]
+#         except:
+#             pass
 
-        try:
-            right = pose_coordinates["RIGHT_HAND"]
-        except:
-            pass
+#         try:
+#             right = pose_coordinates["RIGHT_HAND"]
+#         except:
+#             pass
 
-        print('left', left)
-        print('right', right)
+#         print('left', left)
+#         print('right', right)
 
-        # If less than 2 hands can be seen, stop the robot 
-        if left == [] and right == []:
-            self.command = 'stop'
+#         # If less than 2 hands can be seen, stop the robot 
+#         if left == [] and right == []:
+#             self.command = 'stop'
 
-        # If both hands left, turn left     
-        elif left==[] and right!=[]:
-            self.command = 'right'
+#         # If both hands left, turn left     
+#         elif left==[] and right!=[]:
+#             self.command = 'right'
 
-        elif right==[] and left!=[]:
-            self.command = 'left'
+#         elif right==[] and left!=[]:
+#             self.command = 'left'
 
-        elif left[1] > vertical_th and right[1] > vertical_th:
-            self.command = 'forward'
+#         elif left[1] > vertical_th and right[1] > vertical_th:
+#             self.command = 'forward'
 
-        elif left[1] < vertical_th and right[1] < vertical_th:
-            self.command = 'backward'
+#         elif left[1] < vertical_th and right[1] < vertical_th:
+#             self.command = 'backward'
 
-        print(self.command)
+#         print(self.command)
 
-    def on_tracking_event(self, event):
-        """
-        Code to run whenever a hand is detected 
-        """
-        pose_coordinates = {}
+#     def on_tracking_event(self, event):
+#         """
+#         Code to run whenever a hand is detected 
+#         """
+#         pose_coordinates = {}
 
-        for hand in event.hands:
-            pose_coordinates[str(hand.type).upper()[9:] + "_HAND"] = [round(hand.palm.position.x, 2),
-                                                                      round(hand.palm.position.y, 2),
-                                                                      round(hand.palm.position.z, 2)
-                                                                     ]
+#         for hand in event.hands:
+#             pose_coordinates[str(hand.type).upper()[9:] + "_HAND"] = [round(hand.palm.position.x, 2),
+#                                                                       round(hand.palm.position.y, 2),
+#                                                                       round(hand.palm.position.z, 2)
+#                                                                      ]
 
-            for node_name, coordinates in pose_coordinates.items():
-                print(node_name, '\t', coordinates)
+#             for node_name, coordinates in pose_coordinates.items():
+#                 print(node_name, '\t', coordinates)
 
-            self.hand_coordinates_to_command(pose_coordinates)
+#             self.hand_coordinates_to_command(pose_coordinates)
 
-            if send_command:
-                self.send_command_to_server(HOST, PORT)
+#             if send_command:
+#                 self.send_command_to_server(HOST, PORT)
 
 
 class VideoStreamTracker():
@@ -688,10 +689,10 @@ def track_keys():
         sys.exit(1)
 
 def track_leap_motion():
-    my_listener = LeapMotionTracker()
+    leap_motion_tracker = LeapMotionTracker()
 
     connection = leap.Connection()
-    connection.add_listener(my_listener)
+    connection.add_listener(leap_motion_tracker)
 
     running = True
 
@@ -701,12 +702,12 @@ def track_leap_motion():
         while running:
 
             # If no hand detected, stop robot
-            my_listener.command = 'stop'
-            print(my_listener.command)
+            leap_motion_tracker.command = 'stop'
+            print(leap_motion_tracker.command)
 
             # Send command to robot 
             if send_command:
-                send_command_to_server(HOST, PORT, my_listener.command)
+                send_command_to_server(HOST, PORT, leap_motion_tracker.command)
 
             time.sleep(1)
 
